228abc083339385d33fb5378d373ed8b23355219
Tyler Youngblood <tyleryoungblood@yahoo.com>
2014-09-19 15:21:10 -0400

    fix .input-group-btn and .input-group-addon so that they are formatted properly. Move code popup to center of screen

--- js/copy-to-clipboard.js
+++ js/copy-to-clipboard.js
@@ -1,15 +1,59 @@
 $(function() {
-	$("code").on("click", function() {
-		$.growl('successfully copied to clipboard');
-		//console.log($(this).parent().prev()[0].outerHTML);
-		var target = $(this).data("target");
+
+	function getHtml(codeElement) {
+
+		// get target element if data-target is set on code element
+		var target = $(codeElement).data("target");
 		if(target) {
+			// if data-target, grab element with that target, duplicate (clone), and remove data-target-name attribute
+			// prior to copying to clipboard
 			var el = $("[data-target-name=" + target + "]").clone().removeAttr("data-target-name");
 		} else {
-			var el = $($(this).parent().children("code").text().replace(/\s/g, ""));
+			// create selector from code element inner-html (content) and select the element from the dom 
+			var el = $($(codeElement).parent().children("code").text().replace(/\s/g, "")); 
+			// the (/\s/g, "") regex removes whitespace when multiple classes are included in the same code element
 		}
 
-		console.log(el[0].outerHTML);
+		// stringifys the html grabbed from above (the returned object is turned into a string)
+		return el[0].outerHTML;
+
+	}
+
+	$("code").on("click", function() {
+
+		
+		console.log(getHtml(this));
+		$.growl('successfully copied to clipboard');
+
+		
 
 	})
-})
+
+	// $("code").on("mouseover", function() {
+
+	// 	var tooltip = $("<div/>").addClass("codeTooltip").text(getHtml(this));
+	// 	$(this).after(tooltip);
+
+	// })
+
+	// $("code").on("mouseout", function() {
+
+	// 	$(".codeTooltip").remove();
+
+	// })
+
+	$("code").each(function(){
+		try {
+			var tooltip = $("<div/>").addClass("codeTooltip").text(getHtml(this));
+			$(this).after(tooltip);
+		} catch(e) {
+		
+		}
+
+		
+	})
+
+});
+
+
+
